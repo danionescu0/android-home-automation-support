7cb9a6e3665cb0962743ffeb0d95eed7c1a88b81
Dan Ionescu <Dan Ionescu@localhost>
2016-08-28 14:17:14 +0300

    developing a menu with: main window, settings, about

--- app/src/main/java/homeautomation/dan/ionescu/homeautomation/MainActivity.java
+++ app/src/main/java/homeautomation/dan/ionescu/homeautomation/MainActivity.java
@@ -7,18 +7,40 @@ import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-    static String APP_URL = "http://danionescu0.go.ro:8080/actuator/cici/off";
+    static String APP_URL = "http://danionescu0.go.ro:8080";
     private static final String TAG = "MainActivity";
     boolean alarmManagerStarted = false;
 
     private PendingIntent pendingIntent;
     private AlarmManager alarmManager;
     private Intent gpsTrackerIntent;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.server_settings:
+                showServerSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,5 +74,10 @@ public class MainActivity extends AppCompatActivity {
                 SystemClock.elapsedRealtime(),
                 1 * 60000,
                 pendingIntent);
+    }
+
+    private void showServerSettings() {
+        Intent showServerSettings = new Intent(this, ServerSettingsActivity.class);
+        startActivity(showServerSettings);
     }
 }
---
+++ app/src/main/java/homeautomation/dan/ionescu/homeautomation/ServerSettingsActivity.java
@@ -0,0 +1,13 @@
+package homeautomation.dan.ionescu.homeautomation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServerSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_settings);
+    }
+}
