c95a88ed718852acf9ac236a517bae3f655ff251
Dan Ionescu <ionescu.dan84@gmail.com>
2018-12-08 20:39:14 +0200

    removed padding from the layout so that the webview can expand more

--- app/src/main/java/homeautomation/dan/ionescu/homeautomation/MainActivity.java
+++ app/src/main/java/homeautomation/dan/ionescu/homeautomation/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
